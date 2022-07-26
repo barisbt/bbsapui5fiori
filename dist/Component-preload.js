fee979fbc281a48e96df44210d5fbfdc9036f90f
barisbt <63521382+barisbt@users.noreply.github.com>
2022-07-26 14:28:43 +0300

    initial commit but not stable

---
+++ dist/Component-preload.js
@@ -0,0 +1,6 @@
+//@ui5-bundle solutions/blackbelt/Component-preload.js
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"solutions/blackbelt/manifest.json":'{"sap.app":{"id":"solutions.blackbelt","type":"application"}}'
+}});
